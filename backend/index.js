3bdc71e5b991314b8cdce8137d5210854e38fe10
WelComeRai <155359687+WelComeRai@users.noreply.github.com>
2025-05-02 17:18:28 +0500

    check

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Your on Home page...");
+  res.send("Your on Home page and i change on Pc check it show on laptop...");
 });
 
 app.listen(5000, () => {
