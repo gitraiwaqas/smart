83338b73f63980d47c819c913f30c265e180c218
gitraiwaqas <sunnyraimobile@gmail.com>
2025-05-02 21:02:43 -0700

    add commit in js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
   res.send("Your on Home page and i change on Pc check it show on laptop...");
 });
 
+// Using pull i reverse the code it is good thin about the 
+
 app.listen(5000, () => {
   console.log(`App is runing http://localhost:5000`);
 });
